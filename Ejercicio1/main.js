cc4b08648e9357157cefa02d217e045744954f29
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-11-20 09:14:02 -0500

    Plantilla Taller 4 20 11 2021

---
+++ Ejercicio1/main.js
@@ -0,0 +1,6 @@
+// 1.Pregunto si quiere el huevo frito.
+// Si me dice que si, lo frio, si me dice que no, lo hago hervido.
+// Una vez cocinado le pregunto si quiere sal en el huevo.
+// Si me dice que no lo sirvo en el Plato. Si me dice que si le hecho sal y después lo sirvo en el plato.
+
+console.log("Conexion");
